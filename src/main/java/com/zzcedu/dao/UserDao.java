5ab22a9859d8154ee816f6c7c957c490afb81c04
tom <tom@qq.com>
2020-08-20 17:25:03 +0800

    增加新的API方法：根据用户名查询用户信息

--- src/main/java/com/zzcedu/dao/UserDao.java
+++ src/main/java/com/zzcedu/dao/UserDao.java
@@ -12,4 +12,6 @@ import java.util.List;
 public interface UserDao {
     @Select("select * from cn_user")
     List<User> findAll();
+    @Select("select * from cn_user where cn_user_name = #{name}")
+    public User findByName(String name);
 }
